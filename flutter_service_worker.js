16bcbc822bf5fcac81030abc8878da6d1a48593d
nhoai2524 <ngochoainguyen2502@gmail.com>
2026-03-18 07:14:26 +0700

    Built web
    Branch: main
    Commit: 7e7dcfe586e2963d486d29ffaf0629e6436ec1cb
    
    package:peanut 6.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -64,7 +64,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d98f23b45155194d41a9a0b398eb9a1d
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "01730ccc2118c2cfd7e1fb16a27633cd",
+"flutter_bootstrap.js": "bc925931fb96e8eb7e35d717ebe9da09",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
