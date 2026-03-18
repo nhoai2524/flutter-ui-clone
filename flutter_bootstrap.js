fb0c4f60bc4abb198367bbaa3413aaa1ddca7974
nhoai2524 <ngochoainguyen2502@gmail.com>
2026-03-18 07:02:32 +0700

    Built web
    Branch: main
    Commit: 7e7dcfe586e2963d486d29ffaf0629e6436ec1cb
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "94163530"
+    serviceWorkerVersion: "3483924824"
   }
 });
